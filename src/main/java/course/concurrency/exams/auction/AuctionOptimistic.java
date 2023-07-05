7fadd264885889f72c60694a423eedd88824e1a4
Anton Gavrilov <agav2000@mail.ru>
2023-07-05 17:26:49 +0300

    Optimistic fixed

--- src/main/java/course/concurrency/exams/auction/AuctionOptimistic.java
+++ src/main/java/course/concurrency/exams/auction/AuctionOptimistic.java
@@ -13,15 +13,15 @@ public class AuctionOptimistic implements Auction {
     private final AtomicReference<Bid> latestBid = new AtomicReference<>(new Bid(0L, 0L, 0L));
 
     public boolean propose(Bid bid) {
-        if (bid.getPrice() > latestBid.get().getPrice()) {
-            Bid expectedLatestBid;
-            do {
-                expectedLatestBid = latestBid.get();
-            } while (!latestBid.compareAndSet(expectedLatestBid, bid));
-            notifier.sendOutdatedMessage(expectedLatestBid);
-            return true;
-        }
-        return false;
+        Bid expectedLatestBid;
+        do {
+            expectedLatestBid = latestBid.get();
+            if (bid.getPrice() <= expectedLatestBid.getPrice()) {
+                return false;
+            }
+        } while (!latestBid.compareAndSet(expectedLatestBid, bid));
+        notifier.sendOutdatedMessage(expectedLatestBid);
+        return true;
     }
 
     public Bid getLatestBid() {
