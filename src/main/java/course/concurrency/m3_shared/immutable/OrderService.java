584166859be82c9758091e7ffd414571a6b3506d
Anton Gavrilov <agav2000@mail.ru>
2023-07-17 01:13:44 +0300

    OrderService

--- src/main/java/course/concurrency/m3_shared/immutable/OrderService.java
+++ src/main/java/course/concurrency/m3_shared/immutable/OrderService.java
@@ -1,46 +1,95 @@
 package course.concurrency.m3_shared.immutable;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class OrderService {
 
-    private Map<Long, Order> currentOrders = new HashMap<>();
+    private final ConcurrentHashMap<Long, Order> currentOrders = new ConcurrentHashMap<>();
     private long nextId = 0L;
 
-    private synchronized long nextId() {
+    private long nextId() {
         return nextId++;
     }
 
-    public synchronized long createOrder(List<Item> items) {
+    public long createOrder(List<Item> items) {
         long id = nextId();
-        Order order = new Order(items);
-        order.setId(id);
+        Order order = Order.createOrder(id, items);
         currentOrders.put(id, order);
         return id;
     }
 
-    public synchronized void updatePaymentInfo(long orderId, PaymentInfo paymentInfo) {
-        currentOrders.get(orderId).setPaymentInfo(paymentInfo);
-        if (currentOrders.get(orderId).checkStatus()) {
+    public void updatePaymentInfo(long orderId, PaymentInfo paymentInfo) {
+        Order order = currentOrders.computeIfPresent(orderId, (key, value) -> value.updatedPaymentInfo(paymentInfo));
+        if (order != null && order.checkStatus()) {
             deliver(currentOrders.get(orderId));
         }
     }
 
-    public synchronized void setPacked(long orderId) {
-        currentOrders.get(orderId).setPacked(true);
-        if (currentOrders.get(orderId).checkStatus()) {
+    public void setPacked(long orderId) {
+        Order order = currentOrders.computeIfPresent(orderId, (key, value) -> value.updatedPacked(true));
+        if (order != null && order.checkStatus()) {
             deliver(currentOrders.get(orderId));
         }
     }
 
-    private synchronized void deliver(Order order) {
+    private void deliver(Order order) {
         /* ... */
-        currentOrders.get(order.getId()).setStatus(Order.Status.DELIVERED);
+        currentOrders.computeIfPresent(order.getId(), (key, value) -> value.updatedStatus(Order.Status.DELIVERED));
     }
 
-    public synchronized boolean isDelivered(long orderId) {
+    public boolean isDelivered(long orderId) {
         return currentOrders.get(orderId).getStatus().equals(Order.Status.DELIVERED);
     }
 }
+
+
+
+
+
+//package course.concurrency.m3_shared.immutable;
+//
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//public class OrderService {
+//
+//    private Map<Long, Order> currentOrders = new HashMap<>();
+//    private long nextId = 0L;
+//
+//    private synchronized long nextId() {
+//        return nextId++;
+//    }
+//
+//    public synchronized long createOrder(List<Item> items) {
+//        long id = nextId();
+//        Order order = new Order(items);
+//        order.setId(id);
+//        currentOrders.put(id, order);
+//        return id;
+//    }
+//
+//    public synchronized void updatePaymentInfo(long orderId, PaymentInfo paymentInfo) {
+//        currentOrders.get(orderId).setPaymentInfo(paymentInfo);
+//        if (currentOrders.get(orderId).checkStatus()) {
+//            deliver(currentOrders.get(orderId));
+//        }
+//    }
+//
+//    public synchronized void setPacked(long orderId) {
+//        currentOrders.get(orderId).setPacked(true);
+//        if (currentOrders.get(orderId).checkStatus()) {
+//            deliver(currentOrders.get(orderId));
+//        }
+//    }
+//
+//    private synchronized void deliver(Order order) {
+//        /* ... */
+//        currentOrders.get(order.getId()).setStatus(Order.Status.DELIVERED);
+//    }
+//
+//    public synchronized boolean isDelivered(long orderId) {
+//        return currentOrders.get(orderId).getStatus().equals(Order.Status.DELIVERED);
+//    }
+//}
