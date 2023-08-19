cbc9efb590b5a7f1bca069cd726515a09ac0929d
Anton Gavrilov <agav2000@mail.ru>
2023-08-19 21:02:40 +0300

    Blocking Queue: latch await fix

--- src/test/java/course/concurrency/m5_streams/blockingqueue/ConcurrencyTests.java
+++ src/test/java/course/concurrency/m5_streams/blockingqueue/ConcurrencyTests.java
@@ -12,25 +12,30 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ConcurrencyTests {
 
     @Test
-    void concurrentEnqueueDequeue() throws InterruptedException {
+    void concurrentEnqueueDequeue() {
         int threadsCount = 100;
         int iterations = 1000;
 
         ExecutorService executor = Executors.newFixedThreadPool(threadsCount);
-        CountDownLatch latch = new CountDownLatch(threadsCount);
+        CountDownLatch latch = new CountDownLatch(1);
         Queue<Integer> queue = new Queue<>(Integer.class, threadsCount);
 
         for (int i = 0; i < threadsCount; i++) {
-            executor.submit(new Thread(() -> {
-                latch.countDown();
+            Thread tread = new  Thread(() -> {
+                try {
+                    latch.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
                 for (int j = 0; j < iterations; j++) {
                     queue.enqueue(j);
                     queue.dequeue();
                 }
-            }));
+            });
+            executor.submit(tread);
         }
 
-        latch.await();
+        latch.countDown();
         executor.shutdown();
 
         try {
